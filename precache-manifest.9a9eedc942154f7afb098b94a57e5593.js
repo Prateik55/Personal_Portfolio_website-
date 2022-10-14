913f30f26f2951f94ecb8fa04070d78dd4da4419
Prateik <itspsr5@gmail.com>
2022-10-14 11:41:24 +0530

    Updates

---
+++ precache-manifest.9a9eedc942154f7afb098b94a57e5593.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac07a150ca5b04cbb47a",
+    "url": "/Personal_Portfolio_website-/static/css/main.e1936ae9.chunk.css"
+  },
+  {
+    "revision": "ac07a150ca5b04cbb47a",
+    "url": "/Personal_Portfolio_website-/static/js/main.ac07a150.chunk.js"
+  },
+  {
+    "revision": "f91e43220d8bc309b5b0",
+    "url": "/Personal_Portfolio_website-/static/css/1.c24936ce.chunk.css"
+  },
+  {
+    "revision": "f91e43220d8bc309b5b0",
+    "url": "/Personal_Portfolio_website-/static/js/1.f91e4322.chunk.js"
+  },
+  {
+    "revision": "a9eb9cda1dbaa660ee54",
+    "url": "/Personal_Portfolio_website-/static/js/runtime~main.a9eb9cda.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/Personal_Portfolio_website-/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/Personal_Portfolio_website-/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/Personal_Portfolio_website-/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/Personal_Portfolio_website-/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/Personal_Portfolio_website-/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/Personal_Portfolio_website-/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/Personal_Portfolio_website-/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/Personal_Portfolio_website-/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/Personal_Portfolio_website-/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "1c5b6c207bb284b1968155eea7c76869",
+    "url": "/Personal_Portfolio_website-/index.html"
+  }
+];
